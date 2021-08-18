ac93c05448de001851d35850228daaa82b75d3f1
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-08-18 17:33:49 +0200

    Update Category.tsx

--- src/pages/auth/Category.tsx
+++ src/pages/auth/Category.tsx
@@ -1,6 +1,8 @@
 import Growth from './../../media/business-success-growing-growth-increase-up-concept-wooded-cube-block-with-word-growth_20693-206.jpg'
 import Engage from './../../media/s2e9-engaging-influencers-LJnEWBfbpMj-sta84Bz7q3b.500x500.png'
 import Currency from './../../media/curreny.jpg'
+import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 const categories = [
     {
@@ -19,6 +21,9 @@ const categories = [
 ]
 
 const Category = () => {
+
+    const [clicked, setClicked] = useState(false)
+    
     return (
       <div className="bg-white px-32 py-10 min-h-screen">
           <div className="m-4">
@@ -43,14 +48,24 @@ const Category = () => {
                                     id="remember-me"
                                     name="remember-me"
                                     type="checkbox"
-                                    onChange={() => {}}
+                                    onChange={() => {
+                                        clicked ? setClicked(false) : setClicked(true)
+                                    }}
                                     className="h-3 w-4 dark:bg-transparent m-3 bg-white text-blue-600 focus:ring-blue-500 dark:border-gray-900 border-gray-300 rounded-none"
                                     />
                                     <p className="ml-3 text-white text-md font-medium">{category.name}</p>
                             </div>
                         ))
                     }
+                    {
+                        clicked === true ? <Link to="#" className="justify-end w-full mt-3 text-right text-white font-semibold bg-blue-400 hover:bg-blue-500 transition duration-150 rounded-sm py-3 px-8">
+                            Continue
+                        </Link> : <Link to="#" className="justify-end w-full mt-3 text-right text-white font-semibold bg-blue-400 hover:bg-blue-500 transition duration-150 rounded-sm py-3 px-8">
+                            Skip
+                        </Link>
+                    }
                 </div>
+                
           </div>
       </div>
       )
