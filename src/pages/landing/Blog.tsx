8ab16ebf02c47400c536e8023f343e1863409da0
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-08-20 21:10:56 +0200

    blog

--- src/pages/landing/Blog.tsx
+++ src/pages/landing/Blog.tsx
@@ -1,8 +1,211 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 const Blog = () => {
     return(
-      <div>
-          Blog stuff here
-      </div>)
+      <div className="px-24 py-10">
+          <section className="text-gray-600 body-font">
+              <div className="container px-24 py-5 mx-auto">
+            <div className="flex flex-wrap w-full mb-20">
+
+            <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
+                <h1 className="sm:text-3xl text-2xl font-bold title-font mb-2 text-gray-900 dark:text-gray-300">Boost your reading experience with Malawi's best selling book</h1>
+            </div>
+
+            <p className="lg:w-1/2 w-full leading-relaxed text-gray-500 dark:text-gray-400">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify, subway tile poke farm-to-table. Franzen you probably haven't heard of them man bun deep jianbing selfies heirloom prism food truck ugh squid celiac humblebrag.</p>
+            </div>
+            <div className="w-full relative px-3 mb-6">
+            </div>
+
+        </div>
+            <div className="container px-5 mx-auto">
+                <div className="flex flex-wrap -mx-4 -my-8">
+                <div className="py-8 px-4 lg:w-1/3">
+                    <div className="h-full flex items-start">
+                    <div className="w-12 flex-shrink-0 flex flex-col text-center leading-none">
+                        <span className="text-gray-500 pb-2 mb-2 border-b-2 border-gray-200">Jul</span>
+                        <span className="font-medium text-lg text-gray-800 title-font leading-none">18</span>
+                    </div>
+                    <div className="flex-grow pl-6">
+                        <h2 className="tracking-widest text-xs title-font font-medium text-indigo-500 mb-1">CATEGORY</h2>
+                        <h1 className="title-font text-xl font-medium text-gray-900 mb-3">The 400 Blows</h1>
+                        <p className="leading-relaxed mb-5">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                        <a className="inline-flex items-center">
+                        <img alt="blog" src="https://dummyimage.com/103x103" className="w-8 h-8 rounded-full flex-shrink-0 object-cover object-center"/>
+                        <span className="flex-grow flex flex-col pl-3">
+                            <span className="title-font font-medium text-gray-900">Alper Kamu</span>
+                        </span>
+                        </a>
+                    </div>
+                    </div>
+                </div>
+                <div className="py-8 px-4 lg:w-1/3">
+                    <div className="h-full flex items-start">
+                    <div className="w-12 flex-shrink-0 flex flex-col text-center leading-none">
+                        <span className="text-gray-500 pb-2 mb-2 border-b-2 border-gray-200">Jul</span>
+                        <span className="font-medium text-lg text-gray-800 title-font leading-none">18</span>
+                    </div>
+                    <div className="flex-grow pl-6">
+                        <h2 className="tracking-widest text-xs title-font font-medium text-indigo-500 mb-1">CATEGORY</h2>
+                        <h1 className="title-font text-xl font-medium text-gray-900 mb-3">Shooting Stars</h1>
+                        <p className="leading-relaxed mb-5">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                        <a className="inline-flex items-center">
+                        <img alt="blog" src="https://dummyimage.com/102x102" className="w-8 h-8 rounded-full flex-shrink-0 object-cover object-center"/>
+                        <span className="flex-grow flex flex-col pl-3">
+                            <span className="title-font font-medium text-gray-900">Holden Caulfield</span>
+                        </span>
+                        </a>
+                    </div>
+                    </div>
+                </div>
+                <div className="py-8 px-4 lg:w-1/3">
+                    <div className="h-full flex items-start">
+                    <div className="w-12 flex-shrink-0 flex flex-col text-center leading-none">
+                        <span className="text-gray-500 pb-2 mb-2 border-b-2 border-gray-200">Jul</span>
+                        <span className="font-medium text-lg text-gray-800 title-font leading-none">18</span>
+                    </div>
+                    <div className="flex-grow pl-6">
+                        <h2 className="tracking-widest text-xs title-font font-medium text-indigo-500 mb-1">CATEGORY</h2>
+                        <h1 className="title-font text-xl font-medium text-gray-900 mb-3">Neptune</h1>
+                        <p className="leading-relaxed mb-5">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                        <a className="inline-flex items-center">
+                        <img alt="blog" src="https://dummyimage.com/101x101" className="w-8 h-8 rounded-full flex-shrink-0 object-cover object-center"/>
+                        <span className="flex-grow flex flex-col pl-3">
+                            <span className="title-font font-medium text-gray-900">Henry Letham</span>
+                        </span>
+                        </a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </section>
+            <section className="text-gray-600 body-font">
+            <div className="container px-5 py-5 mx-auto">
+                <div className="flex flex-wrap -mx-4 -my-8">
+                <div className="py-8 px-4 lg:w-1/3">
+                    <div className="h-full flex items-start">
+                    <div className="w-12 flex-shrink-0 flex flex-col text-center leading-none">
+                        <span className="text-gray-500 pb-2 mb-2 border-b-2 border-gray-200">Jul</span>
+                        <span className="font-medium text-lg text-gray-800 title-font leading-none">18</span>
+                    </div>
+                    <div className="flex-grow pl-6">
+                        <h2 className="tracking-widest text-xs title-font font-medium text-indigo-500 mb-1">CATEGORY</h2>
+                        <h1 className="title-font text-xl font-medium text-gray-900 mb-3">The 400 Blows</h1>
+                        <p className="leading-relaxed mb-5">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                        <a className="inline-flex items-center">
+                        <img alt="blog" src="https://dummyimage.com/103x103" className="w-8 h-8 rounded-full flex-shrink-0 object-cover object-center"/>
+                        <span className="flex-grow flex flex-col pl-3">
+                            <span className="title-font font-medium text-gray-900">Alper Kamu</span>
+                        </span>
+                        </a>
+                    </div>
+                    </div>
+                </div>
+                <div className="py-8 px-4 lg:w-1/3">
+                    <div className="h-full flex items-start">
+                    <div className="w-12 flex-shrink-0 flex flex-col text-center leading-none">
+                        <span className="text-gray-500 pb-2 mb-2 border-b-2 border-gray-200">Jul</span>
+                        <span className="font-medium text-lg text-gray-800 title-font leading-none">18</span>
+                    </div>
+                    <div className="flex-grow pl-6">
+                        <h2 className="tracking-widest text-xs title-font font-medium text-indigo-500 mb-1">CATEGORY</h2>
+                        <h1 className="title-font text-xl font-medium text-gray-900 mb-3">Shooting Stars</h1>
+                        <p className="leading-relaxed mb-5">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                        <a className="inline-flex items-center">
+                        <img alt="blog" src="https://dummyimage.com/102x102" className="w-8 h-8 rounded-full flex-shrink-0 object-cover object-center"/>
+                        <span className="flex-grow flex flex-col pl-3">
+                            <span className="title-font font-medium text-gray-900">Holden Caulfield</span>
+                        </span>
+                        </a>
+                    </div>
+                    </div>
+                </div>
+                <div className="py-8 px-4 lg:w-1/3">
+                    <div className="h-full flex items-start">
+                    <div className="w-12 flex-shrink-0 flex flex-col text-center leading-none">
+                        <span className="text-gray-500 pb-2 mb-2 border-b-2 border-gray-200">Jul</span>
+                        <span className="font-medium text-lg text-gray-800 title-font leading-none">18</span>
+                    </div>
+                    <div className="flex-grow pl-6">
+                        <h2 className="tracking-widest text-xs title-font font-medium text-indigo-500 mb-1">CATEGORY</h2>
+                        <h1 className="title-font text-xl font-medium text-gray-900 mb-3">Neptune</h1>
+                        <p className="leading-relaxed mb-5">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                        <a className="inline-flex items-center">
+                        <img alt="blog" src="https://dummyimage.com/101x101" className="w-8 h-8 rounded-full flex-shrink-0 object-cover object-center"/>
+                        <span className="flex-grow flex flex-col pl-3">
+                            <span className="title-font font-medium text-gray-900">Henry Letham</span>
+                        </span>
+                        </a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </section>
+            <section className="text-gray-600 body-font">
+            <div className="container px-5 py-5 mx-auto">
+                <div className="flex flex-wrap -mx-4 -my-8">
+                <div className="py-8 px-4 lg:w-1/3">
+                    <div className="h-full flex items-start">
+                    <div className="w-12 flex-shrink-0 flex flex-col text-center leading-none">
+                        <span className="text-gray-500 pb-2 mb-2 border-b-2 border-gray-200">Jul</span>
+                        <span className="font-medium text-lg text-gray-800 title-font leading-none">18</span>
+                    </div>
+                    <div className="flex-grow pl-6">
+                        <h2 className="tracking-widest text-xs title-font font-medium text-indigo-500 mb-1">CATEGORY</h2>
+                        <h1 className="title-font text-xl font-medium text-gray-900 mb-3">The 400 Blows</h1>
+                        <p className="leading-relaxed mb-5">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                        <a className="inline-flex items-center">
+                        <img alt="blog" src="https://dummyimage.com/103x103" className="w-8 h-8 rounded-full flex-shrink-0 object-cover object-center"/>
+                        <span className="flex-grow flex flex-col pl-3">
+                            <span className="title-font font-medium text-gray-900">Alper Kamu</span>
+                        </span>
+                        </a>
+                    </div>
+                    </div>
+                </div>
+                <div className="py-8 px-4 lg:w-1/3">
+                    <div className="h-full flex items-start">
+                    <div className="w-12 flex-shrink-0 flex flex-col text-center leading-none">
+                        <span className="text-gray-500 pb-2 mb-2 border-b-2 border-gray-200">Jul</span>
+                        <span className="font-medium text-lg text-gray-800 title-font leading-none">18</span>
+                    </div>
+                    <div className="flex-grow pl-6">
+                        <h2 className="tracking-widest text-xs title-font font-medium text-indigo-500 mb-1">CATEGORY</h2>
+                        <h1 className="title-font text-xl font-medium text-gray-900 mb-3">Shooting Stars</h1>
+                        <p className="leading-relaxed mb-5">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                        <a className="inline-flex items-center">
+                        <img alt="blog" src="https://dummyimage.com/102x102" className="w-8 h-8 rounded-full flex-shrink-0 object-cover object-center"/>
+                        <span className="flex-grow flex flex-col pl-3">
+                            <span className="title-font font-medium text-gray-900">Holden Caulfield</span>
+                        </span>
+                        </a>
+                    </div>
+                    </div>
+                </div>
+                <div className="py-8 px-4 lg:w-1/3">
+                    <div className="h-full flex items-start">
+                    <div className="w-12 flex-shrink-0 flex flex-col text-center leading-none">
+                        <span className="text-gray-500 pb-2 mb-2 border-b-2 border-gray-200">Jul</span>
+                        <span className="font-medium text-lg text-gray-800 title-font leading-none">18</span>
+                    </div>
+                    <div className="flex-grow pl-6">
+                        <h2 className="tracking-widest text-xs title-font font-medium text-indigo-500 mb-1">CATEGORY</h2>
+                        <h1 className="title-font text-xl font-medium text-gray-900 mb-3">Neptune</h1>
+                        <p className="leading-relaxed mb-5">Photo booth fam kinfolk cold-pressed sriracha leggings jianbing microdosing tousled waistcoat.</p>
+                        <a className="inline-flex items-center">
+                        <img alt="blog" src="https://dummyimage.com/101x101" className="w-8 h-8 rounded-full flex-shrink-0 object-cover object-center"/>
+                        <span className="flex-grow flex flex-col pl-3">
+                            <span className="title-font font-medium text-gray-900">Henry Letham</span>
+                        </span>
+                        </a>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+            </section>
+      </div>
+      )
 }
 
 export default Blog
