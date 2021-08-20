5209e13fef5c10d05b5f829974d1657285626ee9
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-08-20 11:32:19 +0200

    Update Alerts.tsx

--- src/pages/dashboard/overview/Alerts.tsx
+++ src/pages/dashboard/overview/Alerts.tsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import Alert from "../../../components/Alert"
 import DashboardMain from "../../../components/layouts/DashboardMain"
 
 const alerts = [
@@ -28,12 +29,12 @@ const Alerts = () => {
                         alerts.map((value) => (
                             <div className="bg-white p-3 rounded-sm shadow-sm hover:shadow-md">
                             <div>
-                                <Link to="#" className="p-3 transition duration-150 hover:bg-gray-50 flex justify-justify border-b border-gray-200 pb-2">
+                                <Link onDoubleClick={() => {<Alert icon={''} message='Item deleted successfully'/>}} to="#" className="p-3 transition duration-150 hover:bg-gray-100 flex justify-justify border-b border-gray-200 pb-2">
                                     <div>
                                         <h3 className="font-semibold text-md">{value.title}</h3>
                                         <p>{value.content}</p>
                                     </div>
-                                    <p>{value.time}</p>
+                                    <p className="text-sm w-20">{value.time}</p>
                                 </Link>
                             </div>
                         </div>
