374e1f60b01fa1dbab8f1111333b4ee97768fdd9
NEXTGEN CEO <56794404+KellsWorks@users.noreply.github.com>
2021-09-04 01:18:29 +0200

    Update SubscribersChart.jsx😪

--- src/components/subscribers/SubscribersChart.jsx
+++ src/components/subscribers/SubscribersChart.jsx
@@ -1,55 +1,74 @@
-import ReactApexChart from "react-apexcharts";
+import ReactApexChart from "react-apexcharts"
+
+import { useEffect, useState } from "react";
 
 const SubscribersChart = () => {
+
+  const [data, setData] = useState([])
+
+  useEffect(() => {
+    fetch("http://127.0.0.1:8000/api/v1/statistics/annual-listens",{
+            method: 'get',
+            headers: {'Content-Type':'application/json'}
+            }
+        ).then(async (response) => {
+            return response.json()
+        }).then((data) => {
+            console.log(data)
+            setData(data)
+        }).catch((err) => {
+            console.log(err)
+        })
+  }, [])
         
-    const state = {
-          
-            series: [{
-              name: 'Subscribers',
-              data: [44, 55, 57, 56, 61, 58, 63, 60, 66]
-            }],
-            options: {
-              chart: {
-                type: 'bar',
-                height: 350
+  const state = {
+        
+          series: [{
+            name: 'Subscribers',
+            data: data
+          }],
+          options: {
+            chart: {
+              type: 'bar',
+              height: 350
+            },
+            plotOptions: {
+              bar: {
+                horizontal: false,
+                columnWidth: '55%',
+                endingShape: 'rounded'
               },
-              plotOptions: {
-                bar: {
-                  horizontal: false,
-                  columnWidth: '55%',
-                  endingShape: 'rounded'
-                },
-              },
-              dataLabels: {
-                enabled: false
-              },
-              stroke: {
-                show: true,
-                width: 2,
-                colors: ['transparent']
-              },
-              xaxis: {
-                categories: ['Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct'],
-              },
-              yaxis: {
-                title: {
-                  text: 'Subscribers'
-                }
-              },
-              fill: {
-                opacity: 1
-              },
-              tooltip: {
-                y: {
-                  formatter: function (val) {
-                    return val
-                  }
-                }
+            },
+            dataLabels: {
+              enabled: false
+            },
+            stroke: {
+              show: true,
+              width: 2,
+              colors: ['transparent']
+            },
+            xaxis: {
+              categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+            },
+            yaxis: {
+              title: {
+                text: 'Subscribers'
               }
             },
-          
-          
-        };
+            fill: {
+              opacity: 1
+            },
+            tooltip: {
+              y: {
+                formatter: function (val) {
+                  return val
+                }
+              }
+            }
+          },
+        
+        
+      };
       
 
         return (
